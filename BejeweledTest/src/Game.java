0da4c27f9591ade350a0c88a9ef81f88bdddb1bb
Timo van Leest <T.vanleest@student.tudelft.nl>
2015-09-08 09:59:03 +0200

    some refactoring changes

--- BejeweledTest/src/Game.java
+++ BejeweledTest/src/Game.java
@@ -19,12 +19,11 @@ public class Game extends JPanel implements MouseListener, ActionListener{
 	public static Sounds GameSounds = new Sounds(); // to use the sounds in this class
 	Board board = new Board(8);
 	private int score;
-	private int threeScore = 10;
-	private int fourScore = 15;
+	private int scorePerGem = 10;
 	
 	private Timer timer;
 	private int time;
-	private final int THREETIME = 5;
+	private final int TIMEPERGEM = 5;
 	private final int DELAY = 1000;
 	
 	/**
@@ -68,43 +67,47 @@ public class Game extends JPanel implements MouseListener, ActionListener{
         int row = yvar/40;
         //check if the col and row are inside the board
         if (col < 8 && row < 8 && col >= 0 && row >= 0) {
-        	Game.GameSounds.playAudio("select");
-        	if(board.selectedgem==null){ //first click, select the gem that is clicked
-        		board.selectedgem=board.gems[row][col];
-        		board.gems[row][col].setSelected(true);
-        	}
-        	else //second click, check if the gems are neighbors and can be swapped
-        	{	
-        		board.secondGem = board.gems[row][col];
-        		if (board.areNeighbours(board.selectedgem, board.secondGem)){
-        			board.swap(board.selectedgem.row, board.selectedgem.col, row, col);
-        			boolean first = board.deleteRows(board.selectedgem); 
-        			boolean second = board.deleteRows(board.secondGem);
-            		if (first) {
-            			updateScore();
-            			updateTime();
-            		}
-        			if(first == false && second == false) { //if there are no combinations found after the move
-        				board.swap(board.secondGem.row, board.secondGem.col, row, col); //switches the two switched gems back
-        				//error sound
-        			}
-        		}
-        		else{
-        			//error sound;
-        		}
-        		board.selectedgem.setSelected(false);
-        		board.selectedgem=null; //deselect gem
-        		board.secondGem=null; 
-        	}
-            repaint(); //update the panel
+        	handleMouseClicked(row, col);
         }
     }
+
+	private void handleMouseClicked(int row, int col) {
+//		Game.GameSounds.playAudio("select");
+    	if(board.selectedgem==null){ //first click, select the gem that is clicked
+    		board.selectedgem=board.gems[row][col];
+    		board.gems[row][col].setSelected(true);
+    	}
+    	else //second click, check if the gems are neighbors and can be swapped
+    	{	
+    		board.secondGem = board.gems[row][col];
+    		if (board.areNeighbours(board.selectedgem, board.secondGem)){
+    			board.swap(board.selectedgem.row, board.selectedgem.col, row, col);
+    			boolean first = board.deleteRows(board.selectedgem); 
+    			boolean second = board.deleteRows(board.secondGem);
+        		if (first) {
+        			updateScore();
+        			updateTime();
+        		}
+    			if(first == false && second == false) { //if there are no combinations found after the move
+    				board.swap(board.secondGem.row, board.secondGem.col, row, col); //switches the two switched gems back
+    				//error sound
+    			}
+    		}
+    		else{
+    			//error sound;
+    		}
+    		board.selectedgem.setSelected(false);
+    		board.selectedgem=null; //deselect gem
+    		board.secondGem=null; 
+    	}
+        repaint(); //update the panel		
+	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {}
 	
 	public void updateScore() {
-		score += threeScore;
+		score += scorePerGem;
 	}
 	
 	public int getScore() {
@@ -119,7 +122,7 @@ public class Game extends JPanel implements MouseListener, ActionListener{
 	}
 	
 	public void updateTime() {
-		time += THREETIME;
+		time += TIMEPERGEM;
 	}
 	
 	public int getTime() {
