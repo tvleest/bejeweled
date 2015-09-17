f5356eaaf93e6d9b715011a0a9b3cab282ed0043
Job Zoon <j.zoon@student.tudelft.nl>
2015-09-17 15:30:23 +0200

    Added two different actions to the logger
    And integrated the logger in Main, so all classes can access it

--- src/bejeweled/state/Logger.java
+++ src/bejeweled/state/Logger.java
@@ -1,4 +1,5 @@
 package bejeweled.state;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -6,17 +7,21 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
 
 
 public class Logger {
 	
 	FileWriter writer;
 	File file;
+	String allLogs;
 	
 	public Logger() {
 		Date d = new Date();
 		String date = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss").format(d);
 		file = new File("Logs/" + date);
+		allLogs = "";
 	}
 	
 	public void printText() {
@@ -28,4 +33,26 @@ public class Logger {
 			
 		}
 	}
+	
+	public void updateLogger(String s) {
+		Date d = new Date();
+		String date = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss").format(d);
+		allLogs += date + "  -  " + s + "\n";
+		try {
+			writer = new FileWriter(file);
+			writer.write(allLogs);
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("Something went wrong with the FileWriter in Logger");
+		}
+	}
+	
+	public void close() {
+		try {
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("Something went wrong with the FileWriter in Logger");
+		}
+	}
+	
 }
