0f711a4b61bd50099db09640e445a32b771c3e64
TimYuenior <T.Yue@student.tudelft.nl>
2015-10-08 23:37:19 +0200

    Fix bug where score would reset after continuing game <bug

--- src/bejeweled/Main.java
+++ src/bejeweled/Main.java
@@ -206,7 +206,7 @@ public final class Main extends Application {
 			bejeweled.state.Time time2 = new bejeweled.state.Time(t);
 
 			gamelogic.setTime(time2);
-			gamelogic.setScore(score2);
+			gamelogic.getScoreObject().setScore(score2);
 			boardinstance.setGems(board);
 
 		} catch (FileNotFoundException e) {
